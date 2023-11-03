cb719a00c9bab8412f949f6b7e3ac8e11acc3c8a
NimbleKoala <joelchan414@gmail.com>
2023-11-03 10:50:05 -0500

    first

---
+++ Model.java
@@ -0,0 +1,7 @@
+public class Model {
+    public static void main(String[] args) {
+        int population = 1;
+        int[][] field = new int[100][100];
+
+    }
+}
---
+++ dandelion.java
@@ -0,0 +1,9 @@
+public class dandelion {
+    private int age;
+    private String stage;
+
+    public dandelion() {
+        age = 0;
+        stage = seed;
+    }
+}
