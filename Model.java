14a0f9cf7a647e60e85435be01467bf017e4a303
NimbleKoala <joelchan414@gmail.com>
2023-11-03 11:43:36 -0500

    second

--- Model.java
+++ Model.java
@@ -1,7 +1,17 @@
 public class Model {
     public static void main(String[] args) {
         int population = 1;
-        int[][] field = new int[100][100];
-
+        dandelion[][] field = new dandelion[2000][2000];
+        field[1000][1000] = new dandelion();
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                if (field[i][j] != null) {
+                    field[i][j].increaseAge();
+                    if ()
+                }
+            }
+        }
+        System.out.println(field[1000][1000].getAge());
     }
+
 }
--- dandelion.java
+++ dandelion.java
@@ -1,9 +1,18 @@
 public class dandelion {
     private int age;
     private String stage;
+    private int heads;
 
     public dandelion() {
         age = 0;
-        stage = seed;
+        stage = "seed";
+    }
+
+    public void increaseAge() {
+        this.age++;
+    }
+
+    public int getAge() {
+        return this.age;
     }
 }
